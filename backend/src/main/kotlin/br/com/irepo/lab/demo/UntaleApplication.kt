bd97c0474002235b00df9ac89598897e5af94c5a
Ingrid Silva <ingridsilvatec@outlook.com>
2026-03-04 01:42:12 -0300

    add backend project base

---
+++ backend/src/main/kotlin/br/com/irepo/lab/demo/UntaleApplication.kt
@@ -0,0 +1,11 @@
+package br.com.irepo.lab.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UntaleApplication
+
+fun main(args: Array<String>) {
+	runApplication<UntaleApplication>(*args)
+}
---
+++ backend/src/test/kotlin/br/com/irepo/lab/demo/UntaleApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.irepo.lab.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UntaleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
